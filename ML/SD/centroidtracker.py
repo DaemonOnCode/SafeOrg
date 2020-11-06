8f54f0cf96a92906786376c088946f0cdc6faf18
Daemononcode <daemononcode@gmail.com>
2020-11-07 01:13:26 +0530

    feat(ml/sd): add centroid tracker

---
+++ ML/SD/centroidtracker.py
@@ -0,0 +1,17 @@
+# import the necessary packages
+from scipy.spatial import distance as dist
+from collections import OrderedDict
+import numpy as np
+
+
+class CentroidTracker:
+    def __init__(self, maxDisappeared=50, maxDistance=50):
+    
+
+    def register(self, centroid, inputRect):
+        
+
+    def deregister(self, objectID):
+        
+
+    def update(self, rects):
