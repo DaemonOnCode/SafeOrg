f88fae56c711fc50033fbaeddf1e577c448b5178
Daemononcode <daemononcode@gmail.com>
2020-11-07 03:38:34 +0530

    feat(ml/sd): update centroid tracker

--- ML/SD/centroidtracker.py
+++ ML/SD/centroidtracker.py
@@ -1,17 +1,25 @@
-# import the necessary packages
 from scipy.spatial import distance as dist
 from collections import OrderedDict
 import numpy as np
 
-
 class CentroidTracker:
     def __init__(self, maxDisappeared=50, maxDistance=50):
-    
+        self.nextObjectID = 0
+        self.objects = OrderedDict()
+        self.disappeared = OrderedDict()
+        self.bbox = OrderedDict()
+        self.maxDisappeared = maxDisappeared
+        self.maxDistance = maxDistance    
 
     def register(self, centroid, inputRect):
-        
+        self.objects[self.nextObjectID] = centroid
+        self.bbox[self.nextObjectID] = inputRect
+        self.disappeared[self.nextObjectID] = 0
+        self.nextObjectID += 1 
 
     def deregister(self, objectID):
-        
+        del self.objects[objectID]
+        del self.disappeared[objectID]
+        del self.bbox[objectID]
 
-    def update(self, rects):
+    
