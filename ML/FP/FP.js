2773f3dcba03b10d1a3437c877c7e85f2a0ba6e7
saxenabhishek <as7122000@gmail.com>
2020-11-07 01:14:41 +0530

    Feat(ml/FP) : Starter code

---
+++ ML/FP/FP.js
@@ -0,0 +1,20 @@
+let x = 0;
+
+function setup() {
+  createCanvas(440,440);
+  background(100);
+  
+
+}
+
+start = [30,20]
+
+function draw() {
+  fill(10,10,255)
+  rect(30,30,240,150)
+  fill(255)
+  rect(30,30,70,40)
+  rect(30,30 + 100,150,50)
+  rect(30 + 150,30 + 100,10,10)
+  noloop(); 
+}
