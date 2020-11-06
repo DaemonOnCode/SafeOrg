6fe590fa4a0cd95830b098e3880998f8713e1d43
saxenabhishek <as7122000@gmail.com>
2020-11-07 05:08:06 +0530

    Feat(ml/FP) : Added Resize Functions

--- ML/FP/FP.js
+++ ML/FP/FP.js
@@ -1,4 +1,8 @@
-let x = 0;
+let x = 0,AntipastoPro;
+
+function preload() {
+  AntipastoPro = loadFont('AntipastoPro-Light_trial.ttf');
+}
 
 function setup() {
   createCanvas(600,600);
@@ -6,11 +10,12 @@ function setup() {
 }
 
 
-S = 1.5
+S = 4
 
 function draw() {
   
   textSize(10);
+  textFont(AntipastoPro)
 
   // Big Box
   rect(30*S,30*S,240*S,150*S)
@@ -19,8 +24,12 @@ function draw() {
   rect(30*S,30*S,150*S,140*S)
 
   //
+  fill(255,40,80)
+  //meeting space
   rect(30*S,30*S,80*S,70*S)
+  //creative space
   rect(30*S,(30  + 70)*S,50*S,50*S)
+  fill(255)
 
   rect(30*S,(30 + 120)*S,150*S,50*S)
 
@@ -41,13 +50,13 @@ function draw() {
   
   fill(0);
 
-  text('Meetings/\nCreative Space', 49, 60);
+  text('Meetings/\nCreative Space', 49*S/1.5, 60*S/1.5);
 
-  text('Conference/\nLab', 49, 190);
+  text('Conference/\nLab', 49*S/1.5, 190*S/1.5);
 
-  text('Kitchen and Pantry', 49 , 290)
+  text('Kitchen and Pantry', 49*S/1.5 , 290*S/1.5)
 
-  text('Support', 360 , 190)
+  text('Support', 360*S/1.5 , 190*S/1.5)
 
   noloop(); 
 }
