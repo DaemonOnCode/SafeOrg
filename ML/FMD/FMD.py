faef104733a92289e953588016ab87f59555324c
Daemononcode <daemononcode@gmail.com>
2020-11-07 05:43:29 +0530

    feat(ml/fmd): add face mask detection

---
+++ ML/FMD/FMD.py
@@ -0,0 +1,11 @@
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from imutils.video import VideoStream
+import numpy as np
+import imutils
+import time
+import cv2
+import os
+
+def detect_and_predict_mask(frame, faceNet, maskNet):
