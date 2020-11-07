fc465ac75da8657cf45de9f1b4e871c468e8641a
saxenabhishek <as7122000@gmail.com>
2020-11-08 00:44:51 +0530

    Feat(ml/FMD)  : Extraction of Faulty frames

--- ML/FMD/FMD.py
+++ ML/FMD/FMD.py
@@ -8,43 +8,44 @@ import time
 import cv2
 import os
 
+
 def detect_and_predict_mask(frame, faceNet, maskNet):
-	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),
-		(104.0, 177.0, 123.0))
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),
+                                 (104.0, 177.0, 123.0))
 
-	faceNet.setInput(blob)
-	detections = faceNet.forward()
-	print(detections.shape)
+    faceNet.setInput(blob)
+    detections = faceNet.forward()
 
-	faces = []
-	locs = []
-	preds = []
+    faces = []
+    locs = []
+    preds = []
 
-	for i in range(0, detections.shape[2]):
-		confidence = detections[0, 0, i, 2]
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
 
-		if confidence > 0.5:
-			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-			(startX, startY, endX, endY) = box.astype("int")
+        if confidence > 0.5:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
 
-			(startX, startY) = (max(0, startX), max(0, startY))
-			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+            (startX, startY) = (max(0, startX), max(0, startY))
+            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
 
-			face = frame[startY:endY, startX:endX]
-			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-			face = cv2.resize(face, (224, 224))
-			face = img_to_array(face)
-			face = preprocess_input(face)
+            face = frame[startY:endY, startX:endX]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = cv2.resize(face, (224, 224))
+            face = img_to_array(face)
+            face = preprocess_input(face)
 
-			faces.append(face)
-			locs.append((startX, startY, endX, endY))
+            faces.append(face)
+            locs.append((startX, startY, endX, endY))
 
-	if len(faces) > 0:
-		faces = np.array(faces, dtype="float32")
-		preds = maskNet.predict(faces, batch_size=32)
+    if len(faces) > 0:
+        faces = np.array(faces, dtype="float32")
+        preds = maskNet.predict(faces, batch_size=32)
 
-	return (locs, preds)
+    return (locs, preds)
+
 
 prototxtPath = "ML/FMD/deploy.prototxt"
 weightsPath = "ML/FMD/res10_300x300_ssd_iter_140000.caffemodel"
@@ -52,38 +53,57 @@ faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
 maskNet = load_model("ML/FMD/mask_detector.model")
 
-print("[INFO] starting video stream...")
-cap = cv2.VideoCapture('ML/FMD/DontUse.mp4')
 
-while(cap.isOpened()):
-	ret, frame = cap.read()
+def main(Path):
+    FaultFrames = []
+    cap = cv2.VideoCapture(Path)
 
-	(locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
-
-	for (box, pred) in zip(locs, preds):
-	    (startX, startY, endX, endY) = box
-	    (mask, withoutMask) = pred
-
-	    label = print("Mask") if mask > withoutMask else print("No Mask")
-		#print("Mask") if mask > withoutMask else print("No Mask") 
-		#print("No mask worn") if label == "No Mask" 
-	    color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
-	    label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
-		
-
-		
+    while(cap.isOpened()):
+        ret, frame = cap.read()
 
 
-	    cv2.putText(frame, label, (startX, startY - 10),
-		    cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-	    cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+        if not ret:
+            break
+
+        if int(cap.get(cv2.CAP_PROP_POS_FRAMES)) < 450:
+            continue
+
+        (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+
+        for (box, pred) in zip(locs, preds):
+            (startX, startY, endX, endY) = box
+            (mask, withoutMask) = pred
+
+            label = "Mask" if mask > withoutMask else "No Mask"
+
+            if label == "Mask":
+                FaultFrames.append(int(cap.get(cv2.CAP_PROP_POS_FRAMES)))
+
+            color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+
+            label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+
+            cv2.putText(frame, label, (startX, startY - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+            cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ord("q"):
+            break
+
+    RemFaltyFrames = -1
+
+    if FaultFrames != 0:
+        RemFaltyFrames = [FaultFrames[0]]
+        for i in range(len(FaultFrames) - 1):
+            if(FaultFrames[i] + 1 != FaultFrames[i + 1]):
+                RemFaltyFrames.append(FaultFrames[i+1])
+
+    cv2.destroyAllWindows()
+    return RemFaltyFrames
 
 
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
-
-	if key == ord("q"):
-	    break
-
-cv2.destroyAllWindows()
-vs.stop()
+x = main("ML/FMD/DontUse.mp4")
+print(x)
