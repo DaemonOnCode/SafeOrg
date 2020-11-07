bbd975761ff7934c3bbaae886d7552848ba463af
SaishaShetty <saisha892001@gmail.com>
2020-11-07 18:01:31 +0530

    feat(ml/fmd): frame pickup

--- ML/FMD/FMD.py
+++ ML/FMD/FMD.py
@@ -64,14 +64,20 @@ while(cap.isOpened()):
 	    (startX, startY, endX, endY) = box
 	    (mask, withoutMask) = pred
 
-	    label = "Mask" if mask > withoutMask else "No Mask"
+	    label = print("Mask") if mask > withoutMask else print("No Mask")
+		#print("Mask") if mask > withoutMask else print("No Mask") 
+		#print("No mask worn") if label == "No Mask" 
 	    color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
-
 	    label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+		
+
+		
+
 
 	    cv2.putText(frame, label, (startX, startY - 10),
 		    cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
 	    cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
 
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
